8fc4b4998e6b216ce06c4a45efe5f1c0feb7ef09
Jimmy Lin <usmcjimmy@gmail.com>
2017-07-25 16:38:56 -0400

    added data.json

--- routes/main.js
+++ routes/main.js
@@ -1,15 +1,16 @@
 const express = require('express');
+const data = require('../app/data/data.json');
 
 const main = express.Router();
 
 
 /* api routes */
 main.get('/', (req, res) => {
-  res.render('pages/index');
+  res.render('pages/index', {data: data});
 });
 
 main.get('/services', (req, res) => {
-  res.render('pages/services');
+  res.render('pages/services', {data: data});
 });
 
 main.get('/specials', (req, res) => {
