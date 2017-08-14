146f76e0eda20b36f9fc1b424b5acc42874ca8da
Jimmy Lin <usmcjimmy@gmail.com>
2017-08-14 17:45:35 -0400

    added verification for wechat

--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const data = require('../app/data/data.json');
+const sha1 = require('sha1');
 
 const main = express.Router();
 
@@ -7,6 +8,42 @@ const main = express.Router();
 /* api routes */
 main.get('/', (req, res) => {
   res.render('pages/index', {data: data});
+  const url = req.query
+  const signature = url.signature;
+  const echostr = url.echostr;
+
+  const token = 'abc123456789'
+  const timestamp = url.timestamp;
+  const nonce = url.nonce;
+
+  const longStr = token + timestamp + nonce;
+
+  const tempArry = longStr.split('');
+
+  const alphaArry = tempArry.sort();
+
+  const tempStr = alphaArry.join('');
+
+  const sha1Encrypt = sha1(tempStr);
+
+  // console.log('longStr', longStr);
+  // console.log('tempArry', tempArry);
+  // console.log('alphaArry', alphaArry);
+  // console.log('tempStr', tempStr);
+
+  console.log('signature', signature);
+  // console.log('sha1Encrypt', sha1Encrypt);
+
+  if (url.signature === url.signature) {
+    console.log("true", signature)
+    return true
+  } else {
+    console.log("false")
+    return false
+
+  }
+
+
 });
 
 main.get('/services', (req, res) => {
